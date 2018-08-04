d3056ae8642dac30028eeb124e6bbc8f82eb4c99
boosh <boosh@sugarkube.io>
2018-08-04 13:19:43 +0100

    - Stub out CLI commands

--- cmd/sugarkube/main.go
+++ cmd/sugarkube/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"github.com/boosh/sugarkube/internal/pkg/cmd"
 	"github.com/boosh/sugarkube/internal/pkg/cmd/sugarkube"
 	"os"
 	"path/filepath"
@@ -11,9 +11,7 @@ func main() {
 
 	baseName := filepath.Base(os.Args[0])
 
-	if err := sugarkube.NewCommand(baseName).Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	err := sugarkube.NewCommand(baseName).Execute()
+	cmd.CheckError(err)
 
 }
