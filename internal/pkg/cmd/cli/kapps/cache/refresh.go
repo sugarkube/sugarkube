848623a59aaf0b9d84264a6dbf6c4c8e5e8b2859
boosh <boosh@sugarkube.io>
2018-08-05 21:36:56 +0100

    - Document stub command

--- internal/pkg/cmd/cli/kapps/cache/refresh.go
+++ internal/pkg/cmd/cli/kapps/cache/refresh.go
@@ -1,3 +1,6 @@
 package cache
 
-// Refreshes the kapps cache, but what does that actually mean?
+// Refreshes an existing kapps cache. This could perhaps be merged into a single
+// `cache` command with a flag `--refresh` or `--update` to run in an existing
+// cache directory. I'm not sure we need 2 separate commands that are so
+// similar.
