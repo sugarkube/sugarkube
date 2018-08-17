ca5f0ad16ee7ccc473a196669bd5b2dd72f79dd0
boosh <boosh@sugarkube.io>
2018-08-17 15:45:36 +0100

    - Test deep merging

--- internal/pkg/vars/merge_test.go
+++ internal/pkg/vars/merge_test.go
@@ -9,36 +9,40 @@ import (
 var (
 	topPath  = "./testdata/values.yaml"
 	subPath1 = "./testdata/subdir1/values.yaml"
+	subPath2 = "./testdata/subdir1/subdir2/values.yaml"
 )
 
-func TestMerge(t *testing.T) {
-	topAbsPath, err := filepath.Abs(topPath)
+func getAbsPath(t *testing.T, path string) string {
+	absPath, err := filepath.Abs(path)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	sub1AbsPath, err := filepath.Abs(subPath1)
-	if err != nil {
-		t.Fatal(err)
-	}
+	return absPath
+}
+
+func TestMerge(t *testing.T) {
+	topAbsPath := getAbsPath(t, topPath)
+	sub1AbsPath := getAbsPath(t, subPath1)
+	sub2AbsPath := getAbsPath(t, subPath2)
 
 	tests := []struct {
 		name         string
 		desc         string
 		paths        []string
-		filterKeys   []string
 		expectValues map[string]interface{}
 	}{
 		{
-			name:       "no_merge",
-			desc:       "check loading a single yaml file works",
-			paths:      []string{topAbsPath},
-			filterKeys: []string{"topString", "topBool", "topInt", "topFloat", "sub1"},
+			name:  "no_merge",
+			desc:  "check loading a single yaml file works",
+			paths: []string{topAbsPath},
 			expectValues: map[string]interface{}{
 				"topString": "hello",
 				"topBool":   true,
 				"topInt":    999,
 				"topFloat":  3.14,
+
+				"topIntOvr": 5,
 
 				"sub1": map[interface{}]interface{}{
 					"subString": "subhello1",
@@ -54,15 +58,21 @@ func TestMerge(t *testing.T) {
 			},
 		},
 		{
-			name:       "check_overriding",
-			desc:       "check overriding a single subkey works",
-			paths:      []string{topAbsPath, sub1AbsPath},
-			filterKeys: []string{"topString", "topBool", "topInt", "topFloat", "sub1"},
+			name:  "check_overriding",
+			desc:  "check merging a single level works",
+			paths: []string{topAbsPath, sub1AbsPath},
 			expectValues: map[string]interface{}{
 				"topString": "hello",
 				"topBool":   true,
 				"topInt":    999,
 				"topFloat":  3.14,
+
+				"topIntOvr": 0,
+
+				"subString": "subStr",
+				"subBool":   false,
+				"subInt":    11,
+				"subFloat":  1.11,
 
 				"sub1": map[interface{}]interface{}{
 					"subString": "subhello1",
@@ -77,17 +87,43 @@ func TestMerge(t *testing.T) {
 				},
 			},
 		},
+		{
+			name:  "check_overriding_two",
+			desc:  "check merging two levels deep works",
+			paths: []string{topAbsPath, sub1AbsPath, sub2AbsPath},
+			expectValues: map[string]interface{}{
+				"topString": "hello",
+				"topBool":   true,
+				"topInt":    999,
+				"topFloat":  3.14,
+
+				"topIntOvr": 8,
+
+				"subString": "subStr",
+				"subBool":   false,
+				"subInt":    11,
+				"subFloat":  1.11,
+
+				"sub1": map[interface{}]interface{}{
+					"subString": "subhello1",
+					"subBool":   false,
+					"subInt":    777,
+					"subFloat":  6.22,
+
+					"subStringOvr": "subgoodbye",
+					"subBoolOvr":   false,
+					"subIntOvr":    444,
+					"subFloatOvr":  3.33,
+				},
+
+				"sub2Int": 10,
+			},
+		},
 	}
 
 	for _, test := range tests {
 		result := *Merge(test.paths...)
 
-		filteredResult := map[string]interface{}{}
-
-		for _, k := range test.filterKeys {
-			filteredResult[k] = result[k]
-		}
-
-		assert.Equal(t, test.expectValues, filteredResult, "unexpected merge result for %s", test.name)
+		assert.Equal(t, test.expectValues, result, "unexpected merge result for %s", test.name)
 	}
 }
