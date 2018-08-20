3cbb87788486dc71ff4674318cd83e5de1b2b115
boosh <boosh@sugarkube.io>
2018-08-20 10:32:52 +0100

    - Parse the list of sources from the YAML

--- internal/pkg/kapp/kapp.go
+++ internal/pkg/kapp/kapp.go
@@ -3,8 +3,10 @@ package kapp
 import (
 	"github.com/pkg/errors"
 	"github.com/sugarkube/sugarkube/internal/pkg/acquirer"
+	"github.com/sugarkube/sugarkube/internal/pkg/convert"
 	"github.com/sugarkube/sugarkube/internal/pkg/log"
 	"github.com/sugarkube/sugarkube/internal/pkg/vars"
+	"gopkg.in/yaml.v2"
 )
 
 type installerConfig struct {
@@ -24,14 +26,15 @@ type Kapp struct {
 
 const PRESENT_KEY = "present"
 const ABSENT_KEY = "absent"
+const SOURCES_KEY = "sources"
 
 // Parses a manifest file and returns a list of kapps on success
-func parseManifest(manifest string) ([]Kapp, error) {
-	log.Debugf("Parsing manifest: %s", manifest)
+func parseManifest(manifestPath string) ([]Kapp, error) {
+	log.Debugf("Parsing manifest: %s", manifestPath)
 
 	kapps := make([]Kapp, 0)
 
-	data, err := vars.LoadYamlFile(manifest)
+	data, err := vars.LoadYamlFile(manifestPath)
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
@@ -39,10 +42,36 @@ func parseManifest(manifest string) ([]Kapp, error) {
 	log.Debugf("Loaded manifest data: %#v", data)
 
 	presentKapps := data[PRESENT_KEY]
-	for k, v := range presentKapps.(map[interface{}]interface{}) {
-		log.Debugf("k=%s, v=%#v", k.(string), v)
 
-		// todo - marshal and unmarshal each kapp into a struct then append to the array
+	// parse each kapp definition
+	for k, v := range presentKapps.(map[interface{}]interface{}) {
+		kappName := k.(string)
+		log.Debugf("kappName=%s, v=%#v", kappName, v)
+
+		// parse the list of sources
+		valuesMap, err := convert.MapInterfaceInterfaceToMapStringInterface(v.(map[interface{}]interface{}))
+		if err != nil {
+			return nil, errors.Wrapf(err, "Error converting manifest value to map")
+		}
+
+		// marshal and unmarshal the list of sources
+		sourcesBytes, err := yaml.Marshal(valuesMap[SOURCES_KEY])
+		if err != nil {
+			return nil, errors.Wrapf(err, "Error marshalling sources yaml: %#v", v)
+		}
+
+		log.Debugf("Marshalled sources YAML: %s", sourcesBytes)
+
+		sourcesList := []map[interface{}]interface{}{}
+		err = yaml.UnmarshalStrict(sourcesBytes, &sourcesList)
+		if err != nil {
+			return nil, errors.Wrapf(err, "Error unmarshalling yaml: %s", sourcesBytes)
+		}
+
+		log.Debugf("sourcesList=%#v", sourcesList)
+
+		// now we have a list of sources, get the acquirer for it
+
 	}
 
 	// todo - implement
