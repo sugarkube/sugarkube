d9b8ff7595b61444e11dd3e94cd48be61a7241f9
boosh <boosh@sugarkube.io>
2019-02-24 14:01:33 +0000

    - Make some CLI args required

--- internal/pkg/cmd/cli/cache/create.go
+++ internal/pkg/cmd/cli/cache/create.go
@@ -44,15 +44,17 @@ func newCreateCmd(out io.Writer) *cobra.Command {
 	}
 
 	cmd := &cobra.Command{
-		Use:   "create [cache-dir]",
+		Use:   "create [flags] [stack-file] [stack-name] [cache-dir]",
 		Short: fmt.Sprintf("Create kapp caches"),
 		Long: `Create/update a local kapps cache for a given manifest(s), and renders any 
 templates defined by kapps.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if len(args) == 0 {
-				return errors.New("the path to the kapp cache dir is required")
+			if len(args) < 3 {
+				return errors.New("some required arguments are missing")
 			}
-			c.cacheDir = args[0]
+			c.stackFile = args[0]
+			c.stackName = args[1]
+			c.cacheDir = args[2]
 			return c.run()
 		},
 	}
@@ -60,8 +62,6 @@ templates defined by kapps.`,
 	f := cmd.Flags()
 	f.BoolVar(&c.dryRun, "dry-run", false, "show what would happen but don't create a cluster")
 	f.BoolVar(&c.skipTemplating, "skip-templating", false, "don't render templates for kapps")
-	f.StringVarP(&c.stackName, "stack-name", "n", "", "name of a stack to launch (required when passing --stack-config)")
-	f.StringVarP(&c.stackFile, "stack-config", "s", "", "path to file defining stacks by name")
 
 	return cmd
 }
@@ -82,6 +82,7 @@ func (c *createCmd) run() error {
 	log.Logger.Debugf("Loaded %d init manifest(s) and %d manifest(s)",
 		len(stackConfig.InitManifests), len(stackConfig.Manifests))
 
+	// todo - why is this here? why don't we always validate manifests?
 	for _, manifest := range stackConfig.AllManifests() {
 		err = kapp.ValidateManifest(&manifest)
 		if err != nil {
@@ -89,12 +90,14 @@ func (c *createCmd) run() error {
 		}
 	}
 
+	log.Logger.Debugf("Manifests validated.")
+
 	absCacheDir, err := filepath.Abs(c.cacheDir)
 	if err != nil {
 		return errors.WithStack(err)
 	}
 
-	log.Logger.Debugf("Kapps validated. Caching manifests into %s...", absCacheDir)
+	log.Logger.Debugf("Caching manifests into %s...", absCacheDir)
 
 	// don't use the abs cache path here to keep the output simpler
 	_, err = fmt.Fprintf(c.out, "Caching kapps into '%s'...\n", c.cacheDir)
