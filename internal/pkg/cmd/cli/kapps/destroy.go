87ecf8676706438fd8aba2283d864d44c4140b35
boosh <boosh@sugarkube.io>
2019-03-07 20:10:03 +0000

    - Create a command for destroying kapps

---
+++ internal/pkg/cmd/cli/kapps/destroy.go
@@ -0,0 +1,221 @@
+/*
+ * Copyright 2019 The Sugarkube Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kapps
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"github.com/sugarkube/sugarkube/internal/pkg/cmd/cli/utils"
+	"github.com/sugarkube/sugarkube/internal/pkg/kapp"
+	"github.com/sugarkube/sugarkube/internal/pkg/plan"
+	"io"
+)
+
+type destroyCmd struct {
+	out             io.Writer
+	diffPath        string
+	cacheDir        string
+	dryRun          bool
+	approved        bool
+	oneShot         bool
+	force           bool
+	skipTemplating  bool
+	stackName       string
+	stackFile       string
+	provider        string
+	provisioner     string
+	profile         string
+	account         string
+	cluster         string
+	region          string
+	includeSelector []string
+	excludeSelector []string
+}
+
+func newDestroyCmd(out io.Writer) *cobra.Command {
+	c := &destroyCmd{
+		out: out,
+	}
+
+	cmd := &cobra.Command{
+		Use:   "destroy [flags] [stack-file] [stack-name] [cache-dir]",
+		Short: fmt.Sprintf("Destroy kapps from a cluster"),
+		Long:  `Destroy cached kapps from a target cluster according to manifests.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 3 {
+				return errors.New("some required arguments are missing")
+			} else if len(args) > 3 {
+				return errors.New("too many arguments supplied")
+			}
+			c.stackFile = args[0]
+			c.stackName = args[1]
+			c.cacheDir = args[2]
+			return c.run()
+		},
+	}
+
+	f := cmd.Flags()
+	f.BoolVarP(&c.dryRun, "dry-run", "n", false, "show what would happen but don't create a cluster")
+	f.BoolVar(&c.approved, "approved", false, "actually destroy a cluster diff to install/destroy kapps. If false, kapps "+
+		"will be expected to plan their changes but not make any destrucive changes (e.g. should run 'terraform plan', etc. but not "+
+		"destroy it).")
+	f.BoolVar(&c.oneShot, "one-shot", false, "destroy a cluster diff in a single pass by invoking each kapp with "+
+		"'APPROVED=false' then 'APPROVED=true' to install/destroy kapps in a single invocation of sugarkube")
+	f.BoolVar(&c.force, "force", false, "don't require a cluster diff, just blindly install/destroy all the kapps "+
+		"defined in a manifest(s)/stack config, even if they're already present/absent in the target cluster")
+	f.BoolVarP(&c.skipTemplating, "no-template", "t", false, "skip writing templates for kapps before destroying them")
+	f.StringVarP(&c.diffPath, "diff-path", "d", "", "Path to the cluster diff to destroy. If not given, a "+
+		"diff will be generated")
+	f.StringVar(&c.provider, "provider", "", "name of provider, e.g. aws, local, etc.")
+	f.StringVar(&c.provisioner, "provisioner", "", "name of provisioner, e.g. kops, minikube, etc.")
+	f.StringVar(&c.profile, "profile", "", "launch profile, e.g. dev, test, prod, etc.")
+	f.StringVarP(&c.cluster, "cluster", "c", "", "name of cluster to launch, e.g. dev1, dev2, etc.")
+	f.StringVarP(&c.account, "account", "a", "", "string identifier for the account to launch in (for providers that support it)")
+	f.StringVarP(&c.region, "region", "r", "", "name of region (for providers that support it)")
+	f.StringArrayVarP(&c.includeSelector, "include", "i", []string{},
+		fmt.Sprintf("only process specified kapps (can specify multiple, formatted manifest-id:kapp-id or 'manifest-id:%s' for all)",
+			kapp.WILDCARD_CHARACTER))
+	f.StringArrayVarP(&c.excludeSelector, "exclude", "x", []string{},
+		fmt.Sprintf("exclude individual kapps (can specify multiple, formatted manifest-id:kapp-id or 'manifest-id:%s' for all)",
+			kapp.WILDCARD_CHARACTER))
+	return cmd
+}
+
+func (c *destroyCmd) run() error {
+
+	// todo - pull out the command stuff with apply.go
+
+	// CLI overrides - will be merged with any loaded from a stack config file
+	cliStackConfig := &kapp.StackConfig{
+		Provider:    c.provider,
+		Provisioner: c.provisioner,
+		Profile:     c.profile,
+		Cluster:     c.cluster,
+		Region:      c.region,
+		Account:     c.account,
+	}
+
+	stackConfig, err := utils.BuildStackConfig(c.stackName, c.stackFile, cliStackConfig, c.out)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	dryRunPrefix := ""
+	if c.dryRun {
+		dryRunPrefix = "[Dry run] "
+	}
+
+	var actionPlan *plan.Plan
+
+	if !c.force {
+		panic("Cluster diffing not implemented. Pass --force")
+
+		if c.diffPath != "" {
+			// todo load a cluster diff from a file
+
+			// todo - validate that the embedded stack config matches the target cluster.
+
+			// in future we may want to be able to work entirely from a cluster
+			// diff, in which case it'd really be a plan for us
+			if len(stackConfig.Manifests) > 0 {
+				// todo - validate that the cluster diff matches the manifests, e.g. that
+				// the versions of kapps in the manifests match the versions in the cluster
+				// diff
+			}
+		} else {
+			// todo - create a cluster diff based on stackConfig.Manifests
+		}
+
+		// todo - diff the cache against the kapps in the cluster diff and abort if
+		// it's out-of-sync (unless flags are set to ignore cache changes), e.g.:
+		//cacheDiff, err := cacher.DiffKappCache(clusterDiff, c.cacheDir)
+		//if err != nil {
+		//	return errors.WithStack(err)
+		//}
+		//if len(diff) != 0 {
+		//	return errors.New("Cache out-of-sync with manifests: %s", diff)
+		//}
+
+		// todo - create an action plan from the validated cluster diff
+		//actionPlan, err := plan.FromDiff(clusterDiff)
+
+	} else {
+		_, err = fmt.Fprintf(c.out, "%sPlanning operations on kapps\n", dryRunPrefix)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		// force mode, so no need to perform validation. Just create a reverse plan
+		actionPlan, err = plan.Create(false, stackConfig, stackConfig.Manifests,
+			c.cacheDir, c.includeSelector, c.excludeSelector, !c.skipTemplating)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		// todo - print out the plan
+	}
+
+	if !c.oneShot {
+		_, err = fmt.Fprintf(c.out, "%sApplying the plan to destroy kapps with APPROVED=%#v...\n",
+			dryRunPrefix, c.approved)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		// run the plan either preparing or destroying changes
+		err := actionPlan.Run(c.approved, c.dryRun)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+	} else {
+		_, err = fmt.Fprintf(c.out, "%sApplying the plan in a single pass\n", dryRunPrefix)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		_, err = fmt.Fprint(c.out, "%sFirst applying the plan with APPROVED=false for "+
+			"kapps to plan their changes...\n", dryRunPrefix)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		// one-shot mode, so prepare and destroy the plan straight away
+		err = actionPlan.Run(false, c.dryRun)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		_, err = fmt.Fprintf(c.out, "%sNow running with APPROVED=true to "+
+			"actually destroy changes...\n", dryRunPrefix)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		err = actionPlan.Run(true, c.dryRun)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+	}
+
+	_, err = fmt.Fprintf(c.out, "%sKapp change plan successfully applied\n", dryRunPrefix)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	return nil
+}
--- internal/pkg/cmd/cli/kapps/kapps.go
+++ internal/pkg/cmd/cli/kapps/kapps.go
@@ -33,6 +33,7 @@ func NewKappsCmds(out io.Writer) *cobra.Command {
 	cmd.AddCommand(
 		newTemplateCmd(out),
 		newApplyCmd(out),
+		newDestroyCmd(out),
 		newVarsCmd(out),
 	)
 
