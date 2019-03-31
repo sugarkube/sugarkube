23e40428fdd878511732c6bea06f2a63b4ca3215
boosh <boosh@sugarkube.io>
2019-03-31 09:00:28 +0100

    - Fix stack test

--- internal/pkg/stack/stack_test.go
+++ internal/pkg/stack/stack_test.go
@@ -17,7 +17,12 @@
 package stack
 
 import (
+	"github.com/stretchr/testify/assert"
+	"github.com/sugarkube/sugarkube/internal/pkg/constants"
 	"github.com/sugarkube/sugarkube/internal/pkg/log"
+	"github.com/sugarkube/sugarkube/internal/pkg/registry"
+	"github.com/sugarkube/sugarkube/internal/pkg/structs"
+	"testing"
 )
 
 func init() {
@@ -25,81 +30,81 @@ func init() {
 }
 
 // Test that registry values override values when returning templated vars
-//func TestTemplatedVarsWithRegistry(t *testing.T) {
-//
-//	expectedVarsBlankRegistry := map[string]interface{}{
-//		"sugarkube": map[interface{}]interface{}{
-//			"defaultVars": []interface{}{"", "testAccount", "", "", "testRegion"}},
-//		"someKey1":                      "valueA",
-//		"someKey2":                      "valueB",
-//		"someKey3":                      "valueC",
-//		constants.RegistryKeyKubeConfig: "",
-//		"stack": map[interface{}]interface{}{
-//			"filePath":    "",
-//			"name":        "",
-//			"profile":     "",
-//			"provider":    "",
-//			"provisioner": "",
-//			"region":      "testRegion",
-//			"account":     "testAccount",
-//			"cluster":     "",
-//		},
-//	}
-//
-//	expectedVarsUpdatedRegistry := map[string]interface{}{
-//		"sugarkube": map[interface{}]interface{}{
-//			"defaultVars": []interface{}{"", "testAccount", "", "", "testRegion"}},
-//		"someKey1":                      "valueA",
-//		"someKey2":                      "updatedValue",
-//		"someKey3":                      "valueC",
-//		constants.RegistryKeyKubeConfig: "",
-//		"stack": map[interface{}]interface{}{
-//			"filePath":    "",
-//			"name":        "",
-//			"profile":     "",
-//			"provider":    "",
-//			"provisioner": "",
-//			"region":      "testRegion",
-//			"account":     "testAccount",
-//			"cluster":     "",
-//		},
-//	}
-//
-//	registryObj := registry.NewRegistry()
-//
-//	stackConfig := &StackConfig{
-//		stackFile: structs.Stack{
-//			Region:  "testRegion",
-//			Account: "testAccount",
-//		},
-//	}
-//
-//	stackConfig.SetProviderVars(map[string]interface{}{
-//		"someKey1": "valueA",
-//		"someKey2": "valueB",
-//		"someKey3": "valueC",
-//	})
-//
-//	stackObj := &Stack{
-//		Config:      stackConfig,
-//		Provider:    nil,
-//		Provisioner: nil,
-//		status: &ClusterStatus{
-//			isOnline:              false,
-//			isReady:               false,
-//			sleepBeforeReadyCheck: 0,
-//			startedThisRun:        false,
-//		},
-//		registry: &registryObj,
-//	}
-//
-//	templatedVars, err := stackObj.TemplatedVars(nil, map[string]interface{}{})
-//	assert.Nil(t, err)
-//	assert.Equal(t, expectedVarsBlankRegistry, templatedVars)
-//
-//	// update the registry to override a value
-//	registryObj.SetString("someKey2", "updatedValue")
-//	templatedVars, err = stackObj.TemplatedVars(nil, map[string]interface{}{})
-//	assert.Nil(t, err)
-//	assert.Equal(t, expectedVarsUpdatedRegistry, templatedVars)
-//}
+func TestTemplatedVarsWithRegistry(t *testing.T) {
+
+	expectedVarsBlankRegistry := map[string]interface{}{
+		"sugarkube": map[interface{}]interface{}{
+			"defaultVars": []interface{}{"", "", "testAccount", "", "", "testRegion"}},
+		"someKey1":                      "valueA",
+		"someKey2":                      "valueB",
+		"someKey3":                      "valueC",
+		constants.RegistryKeyKubeConfig: "",
+		"stack": map[interface{}]interface{}{
+			"filePath":    "",
+			"name":        "",
+			"profile":     "",
+			"provider":    "",
+			"provisioner": "",
+			"region":      "testRegion",
+			"account":     "testAccount",
+			"cluster":     "",
+		},
+	}
+
+	expectedVarsUpdatedRegistry := map[string]interface{}{
+		"sugarkube": map[interface{}]interface{}{
+			"defaultVars": []interface{}{"", "", "testAccount", "", "", "testRegion"}},
+		"someKey1":                      "valueA",
+		"someKey2":                      "updatedValue",
+		"someKey3":                      "valueC",
+		constants.RegistryKeyKubeConfig: "",
+		"stack": map[interface{}]interface{}{
+			"filePath":    "",
+			"name":        "",
+			"profile":     "",
+			"provider":    "",
+			"provisioner": "",
+			"region":      "testRegion",
+			"account":     "testAccount",
+			"cluster":     "",
+		},
+	}
+
+	registryObj := registry.New()
+
+	stackConfig := &StackConfig{
+		stackFile: structs.StackFile{
+			Region:  "testRegion",
+			Account: "testAccount",
+		},
+	}
+
+	stackConfig.SetProviderVars(map[string]interface{}{
+		"someKey1": "valueA",
+		"someKey2": "valueB",
+		"someKey3": "valueC",
+	})
+
+	stackObj := &Stack{
+		config:      stackConfig,
+		provider:    nil,
+		provisioner: nil,
+		status: &ClusterStatus{
+			isOnline:              false,
+			isReady:               false,
+			sleepBeforeReadyCheck: 0,
+			startedThisRun:        false,
+		},
+		registry: &registryObj,
+	}
+
+	templatedVars, err := stackObj.GetTemplatedVars(nil, map[string]interface{}{})
+	assert.Nil(t, err)
+	assert.Equal(t, expectedVarsBlankRegistry, templatedVars)
+
+	// update the registry to override a value
+	registryObj.SetString("someKey2", "updatedValue")
+	templatedVars, err = stackObj.GetTemplatedVars(nil, map[string]interface{}{})
+	assert.Nil(t, err)
+	assert.Equal(t, expectedVarsUpdatedRegistry, templatedVars)
+}
