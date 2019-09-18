9fb52e0bc0373cffc4380d91f579e96e08732eee
boosh <boosh@sugarkube.io>
2019-09-18 22:18:28 +0100

    - Add a flag to the version command to just print the version and quit. It could be used to create versioned URLs.

--- internal/pkg/cmd/cli/version.go
+++ internal/pkg/cmd/cli/version.go
@@ -22,19 +22,32 @@ import (
 	"github.com/sugarkube/sugarkube/internal/pkg/version"
 )
 
+type versionConfig struct {
+	concise bool
+}
+
 func newVersionCommand() *cobra.Command {
-	c := &cobra.Command{
+	c := &versionConfig{}
+
+	command := &cobra.Command{
 		Use:   "version",
 		Short: "Print the version number of sugarkube",
 		Long:  `All software has versions. This is sugarkube's.`,
 		Run: func(command *cobra.Command, args []string) {
-			fmt.Println("Build Date:", version.BuildDate)
-			fmt.Println("Git Commit:", version.GitCommit)
-			fmt.Println("Version:", version.Version)
-			fmt.Println("Go Version:", version.GoVersion)
-			fmt.Println("OS / Arch:", version.OsArch)
+			if c.concise {
+				fmt.Printf(version.Version)
+			} else {
+				fmt.Println("Build Date:", version.BuildDate)
+				fmt.Println("Git Commit:", version.GitCommit)
+				fmt.Println("Version:", version.Version)
+				fmt.Println("Go Version:", version.GoVersion)
+				fmt.Println("OS / Arch:", version.OsArch)
+			}
 		},
 	}
 
-	return c
+	f := command.Flags()
+	f.BoolVarP(&c.concise, "concise", "c", false, "only print the version")
+
+	return command
 }
